0294d7c435e07fb4bcaa6f6474164a10d8e9dcfb
Christopher Hermance <42744794+CompMage@users.noreply.github.com>
2023-03-21 15:20:09 -0400

    Create Request-WebCert.ps1

---
+++ Request-WebCert.ps1
@@ -0,0 +1,11 @@
+#Web Cert Template - You may need to request the exact name from your CA Admin with "Certutil -Templates" Use the Name before the :
+$CertTemplateName = "MECM_Web_Cert"
+
+#Gets Host Name
+$ME = ([System.Net.Dns]::GetHostByName($env:computerName)).HostName
+
+#Sets Working Cert Location to LocalMachine Personal Store
+cd Cert:\LocalMachine\My
+
+#Request the Web Cert
+Get-Certificate -Template $CertTemplateName -Url ldap: -DnsName $ME -SubjectName CN=$MY
